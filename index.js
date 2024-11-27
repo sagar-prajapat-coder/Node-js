36aaea1f54db6ac1d7a5f07e78ad7ed3c6f70355
sagar7378 <sagarprajapat250@gmail.com>
2024-11-27 22:05:55 +0530

    routing learning

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // const http = require('http');
 // const colors = require('colors');
 // const data = require('./data');
+const express = require('express');
 // http.createServer((req, res) => {
 //    res.writeHead(200,{'content-type':'application\json'});
 //    res.write(JSON.stringify(data));
@@ -9,12 +10,66 @@
 // }).listen(4500);
 
 // file system 
-const fs = require('fs');
+// const fs = require('fs');
+// const path = require('path');
+// const dirpath = path.join(__dirname,'Testfiles');
+
+// for(i=0;i<5;i++){
+
+//    fs.writeFileSync(dirpath+"/Test"+i+".txt",'hello this is test file');
+// }
+
+
+// Asynchronous progaming handle data using promise
+// let a = 20;
+// let b = 0;
+
+
+// let waitingdata = new Promise((resolve,reject) =>{
+
+//    setTimeout(()=>{
+//       resolve(80)
+//    },2000)
+
+// })
+
+// waitingdata.then((data)=>{
+//    b=data;
+//    console.log(a+b);
+// })
+
+
+
+// use express js and routing
+const app = express();
+// app.get('',(req,res)=>{
+
+//    res.send('Hello this is home page'+' '+req.query.name);
+// })
+
+// app.get('/contact',(req,res)=>{
+//    res.send(`
+//       <input type="text" value="${req.query.name}"><button type="submit">Save</button>
+//       `);
+// })
+
+
+
+// html page folder access
+
 const path = require('path');
-const dirpath = path.join(__dirname,'Testfiles');
+const publicpath = path.join(__dirname,'html');
 
-for(i=0;i<5;i++){
+app.get('',(_,resp)=>{
+   resp.sendFile(`${publicpath}/index.html`);
+});
 
-   fs.writeFileSync(dirpath+"/Test"+i+".txt",'hello this is test file');
-}
+app.get('/about',(_,resp)=>{
+   resp.sendFile(`${publicpath}/about.html`);
+});
 
+app.get('*',(_,resp)=>{
+   resp.sendFile(`${publicpath}/404.html`);
+});
+// app.use(express.static(publicpath));
+app.listen('4000');
